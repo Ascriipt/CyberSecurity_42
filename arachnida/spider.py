4baff73a76b01c8a967b8661c72aa55ab0bd667f
maparigi <maparigi@student.42.fr>
2024-09-09 17:46:09 +0200

    spider not tested maybe finished

--- arachnida/spider.py
+++ arachnida/spider.py
@@ -1,11 +1,13 @@
-from bs4 import BeautifulSoup
+#!/usr/bin/env python
+
 from urllib.parse import urljoin, urlparse
+from bs4 import BeautifulSoup
+from tqdm import tqdm
 
 import validators
 import requests
 import argparse
 import random
-import lxml
 import sys
 import os
 
@@ -19,30 +21,32 @@ parser.add_argument("-p", type=str, help="Path to save downloaded images [./data
 args = parser.parse_args()
 
 if not validators.url(args.URL):
-	sys.exit("Error: Invalid URL format.")
+    sys.exit("Error: Invalid URL format.")
 
 USER_AGENTS = [
-"Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Win64; x64; Trident/5.0; .NET CLR 3.5.30729; .NET CLR 3.0.30729; .NET CLR 2.0.50727; Media Center PC 6.0)",
-"Mozilla/5.0 (compatible; MSIE 8.0; Windows NT 6.0; Trident/4.0; WOW64; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; .NET CLR 1.0.3705; .NET CLR 1.1.4322)",
-"Mozilla/4.0 (compatible; MSIE 7.0b; Windows NT 5.2; .NET CLR 1.1.4322; .NET CLR 2.0.50727; InfoPath.2; .NET CLR 3.0.04506.30)",
-"Mozilla/5.0 (Windows; U; Windows NT 5.1; zh-CN) AppleWebKit/523.15 (KHTML, like Gecko, Safari/419.3) Arora/0.3 (Change: 287 c9dfb30)",
-"Mozilla/5.0 (X11; U; Linux; en-US) AppleWebKit/527+ (KHTML, like Gecko, Safari/419.3) Arora/0.6",
-"Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.2pre) Gecko/20070215 K-Ninja/2.1.1",
-"Mozilla/5.0 (Windows; U; Windows NT 5.1; zh-CN; rv:1.9) Gecko/20080705 Firefox/3.0 Kapiko/3.0",
-"Mozilla/5.0 (X11; Linux i686; U;) Gecko/20070322 Kazehakase/0.4.5"
+    "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Win64; x64; Trident/5.0; .NET CLR 3.5.30729; .NET CLR 3.0.30729; .NET CLR 2.0.50727; Media Center PC 6.0)",
+    "Mozilla/5.0 (compatible; MSIE 8.0; Windows NT 6.0; Trident/4.0; WOW64; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; .NET CLR 1.0.3705; .NET CLR 1.1.4322)",
+    "Mozilla/4.0 (compatible; MSIE 7.0b; Windows NT 5.2; .NET CLR 1.1.4322; .NET CLR 2.0.50727; InfoPath.2; .NET CLR 3.0.04506.30)",
+    "Mozilla/5.0 (Windows; U; Windows NT 5.1; zh-CN) AppleWebKit/523.15 (KHTML, like Gecko, Safari/419.3) Arora/0.3 (Change: 287 c9dfb30)",
+    "Mozilla/5.0 (X11; U; Linux; en-US) AppleWebKit/527+ (KHTML, like Gecko, Safari/419.3) Arora/0.6",
+    "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.2pre) Gecko/20070215 K-Ninja/2.1.1",
+    "Mozilla/5.0 (Windows; U; Windows NT 5.1; zh-CN; rv:1.9) Gecko/20080705 Firefox/3.0 Kapiko/3.0",
+    "Mozilla/5.0 (X11; Linux i686; U;) Gecko/20070322 Kazehakase/0.4.5"
 ]
 
 def spyder(curr_url: str, current_depth=0):
+    print(f"Currently in {curr_url}")
     try:
-        page = requests.get(curr_url, headers={'User-Agent': random.choice(USER_AGENTS)}, timeout=6, allow_redirects=False, stream=True)
+        page = requests.get(curr_url, stream=True, headers={'User-Agent': random.choice(USER_AGENTS)}, timeout=6, allow_redirects=False)
     except requests.RequestException:
         return
 
     try:
-        os.makedirs(args.p)
+        os.makedirs(args.p, exist_ok=True)
     except OSError as e:
         pass
-    soup = BeautifulSoup(page.text, "lxml")
+
+    soup = BeautifulSoup(page.text, "html.parser")
 
     images = soup.find_all('img')
     valid_extensions = ('.jpg', '.jpeg', '.png', '.gif', '.bmp')
@@ -55,23 +59,27 @@ def spyder(curr_url: str, current_depth=0):
             hp = urljoin(curr_url, i)
             baseURL = urlparse(curr_url).scheme + "://" + urlparse(curr_url).netloc
             if f"{hp}".startswith(baseURL) and not curr_url == hp:
-                if not hp in previously_in_spider:
+                if hp not in previously_in_spider:
                     previously_in_spider.append(hp)
-                    print(f"=========================I am in: {curr_url}, going to {hp}, the depth is {current_depth}=========================")
                     spyder(hp, current_depth + 1)
 
-    for url in image_urls:
+    for url in tqdm(image_urls, desc="Downloading images", unit="file"):
         image_url = urljoin(curr_url, url)
-        
+
         try:
-            image_data = requests.get(image_url, headers={'User-Agent': random.choice(USER_AGENTS)}, timeout=6, allow_redirects=False, stream=True).content
+            image_data = requests.get(image_url, stream=True, headers={'User-Agent': random.choice(USER_AGENTS)}, timeout=6, allow_redirects=False).content
             image_name = os.path.basename(image_url)
             image_path = os.path.join(args.p, image_name)
-            
+
             with open(image_path, 'wb') as f:
                 f.write(image_data)
-        
+
         except requests.RequestException as e:
             print(f"Failed to download {url}: {e}")
 
-spyder(args.URL)
+if __name__ == "__main__":
+    try:
+        spyder(args.URL)
+    except KeyboardInterrupt:
+        print("\nProcess interrupted. Exiting gracefully...")
+        sys.exit(0)
